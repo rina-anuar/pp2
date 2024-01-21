d2242fb659172c27e601617f80d4a1739f3de528
Айшуак Ануар <ajsuakanuar@MacBook-Air-Ajsuak.local>
2024-01-21 16:34:39 +0600

    added files from 5 to 30

---
+++ lab1/lab1_exercises/17.py
@@ -0,0 +1,3 @@
+x = {"name" : "Tair", "age" : 36}
+print(type(x))
+#dict
--- lab1/lab1_exercises/a.py
+++
@@ -1,6 +0,0 @@
-x = str(3)    # x will be '3'
-print(x)
-y = int(3)    # y will be 3
-print(y)
-z = float(3)  # z will be 3.0
-print(z)
