4e74b899c6f690548aa6df8b85838714b99cc07a
rinaanuar@Air-Rina <anuarqyzy2@gmail.com>
2024-03-16 14:31:53 +0500

    upd

--- lab7/clock/clock.py
+++ lab7/clock/clock.py
@@ -17,15 +17,6 @@ while True:
     screen.blit(back ,(0, 0))
     now = time.localtime()
 
-    minute_angle = 360 - (now.tm_min * 6)
-    min_rotate = pygame.transform.rotate(minutes, minute_angle)
-    min_pos = ((size[0] - min_rotate.get_width())/2, (size[1] - min_rotate.get_width())/2)
-    screen.blit(min_rotate, min_pos)
-
-    second_angle = 360 - (now.tm_sec * 6)
-    sec_rotate = pygame.transform.rotate(seconds, second_angle)
-    sec_pos = ((size[0] - sec_rotate.get_width())/2, (size[1] - sec_rotate.get_width())/2)
-    screen.blit(sec_rotate, sec_pos)
 
     pygame.display.flip()
 
